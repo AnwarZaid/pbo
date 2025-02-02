9f73c25621e31e9e1df90f8ea5d5009b058b3755
AnwarZaid <muhammadzaidanwar17@gmail.com>
2025-02-02 20:48:44 +0700

    idz progres

--- uas/idzfrelance/WebFreelance/settings.py
+++ uas/idzfrelance/WebFreelance/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "WebFreelance.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'template'],  # Added template directory
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
