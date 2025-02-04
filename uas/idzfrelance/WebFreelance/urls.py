a30f804b9bd10df220db4e3eee49402d52b8ca12
AnwarZaid <muhammadzaidanwar17@gmail.com>
2025-02-04 15:19:20 +0700

    final

--- uas/idzfrelance/WebFreelance/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import index  # Import the index view
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", index, name="index"),  # Add URL pattern for the index view
-]
